1482ebb19c71a8c64526c1995469b80385948851
hogehiko <takrockjp@gmail.com>
2014-04-14 18:59:58 +0900

    4章の問題をとく

--- chapter_4/main.scm
+++ chapter_4/main.scm
@@ -7,7 +7,7 @@
 ;4.1.1 評価機の中核
 
 ;eval
-(define (eval exp env)
+(define (eval-1 exp env)
   
   (cond ((self-evaluating? exp) exp)
         ((variable? exp) (lookup-variable-value exp env))
@@ -209,7 +209,88 @@
 ;Q4.2 eval内のcondの順番を買え、手続き作用の説が代入の説の前に現れるようにしようと考えた。なにがダメ？
 
 ;Q4.3 データ主導流で振り分けできるようにevalを書き直せ。
+(define (make-table)
+  (let ((local-table (list '*table*)))
+    (define (lookup key-1 key-2)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record 
+                  (cdr record)
+                  false))
+            false)))
+    (define (insert! key-1 key-2 value)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (set-cdr! record value)
+                  (set-cdr! subtable
+                            (cons (cons key-2 value)
+                                  (cdr subtable)))))
+            (set-cdr! local-table
+                      (cons (list key-1
+                                  (cons key-2 value))
+                            (cdr local-table)))))
+      'ok)
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc!) insert!)
+            (else (error "Unknown operation --TABLE" m))))
+    dispatch))
 
+(define operation-table (make-table))
+(define get (operation-table 'lookup-proc))
+(define put (operation-table 'insert-proc!))
+
+(define (eval-2 exp env)
+  (let ((proc (get-proc exp)))
+    ((cond ((self-evaluating? exp) exp)
+           ((variable? exp) (lookup-variable-value exp env))
+           ;        ((quoterd? exp) (text-of-quotation exp))
+           ;        ((assignment? exp) (eval-assignment exp env))
+           ;        ((definition? exp) (eval-definition exp env))
+           ;        ((if? exp) (eval-if exp env))
+           ;        ((lambda? exp)
+           ;         (make-procedure (lambda-parameters exp)
+           ;                         (lambda-body exp)
+           ;                         env))
+           ;        ((begin? exp)
+           ;         (eval-sequence (begin-actions exp) env))
+           ;        ((cond? exp) (eval (cond->if exp) env))
+           (proc (apply-in-underlying-scheme proc (list exp env)))
+           ((application? exp)
+            (apply-e (eval (operator exp) env)
+                     (list-of-values (operands exp) env)))
+           (else
+            (error "Unknown expression type --EVAL" exp))))))
+
+  
+;(define eval eval-1)
+(define eval eval-2)
+  
+(define (get-proc exp)
+  (if (tag-of exp) (get (tag-of exp) 'lisp-exp) false))
+
+(define (tag-of exp)
+  (if (pair? exp)
+      (car exp)
+      false))
+
+(put 'quote 'lisp-exp (lambda (exp env) (text-of-quotation exp)))
+(put 'set! 'lisp-exp eval-assignment)
+(put 'define 'lisp-exp eval-definition)
+(put 'if 'lisp-exp eval-if)
+(put 'lambda 'lisp-exp (lambda (exp env)
+                         (make-procedure (lambda-parameters exp)
+                                         (lambda-body exp)
+                                         env)))
+(put 'begin  'lisp-exp (lambda (exp env)
+                         (eval-sequence (begin-actions exp) env)))
+(put 'cond 'lisp-exp (lambda (exp env) (eval (cond->if exp) env)))
+
+  
+    
 ;Q4.4  eval-and eval-orの実装してくみこめ
 
 ;Q4.5 condのもうひとつの構文 (<test> => <recipient>)が評価できるようにせよ
